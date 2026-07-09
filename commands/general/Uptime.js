1dfd45e98643b9ad0dec127e09174bdca0abd96a
CEO-CODEX <ceocodex8@gmail.com>
2026-07-09 20:58:16 +0100

    Create Uptime.js

---
+++ commands/general/Uptime.js
@@ -0,0 +1,20 @@
+const os = require('os');
+
+module.exports = {
+    name: 'uptime',
+    alias: ['up'],
+    desc: 'Check how long the bot has been running',
+    category: 'Bot',
+    reactions: { start: '', success: '' },
+
+    execute: async (sock, m, { reply }) => {
+        const up  = process.uptime();
+        const d   = Math.floor(up / 86400);
+        const h   = Math.floor((up % 86400) / 3600);
+        const min = Math.floor((up % 3600) / 60);
+        const s   = Math.floor(up % 60);
+
+        // Separating the normal text from the backticked time completely
+        await reply('```❁ UPTIME !```' + `${d}d-${h}h-${min}m-${s}s` + '`');
+    }
+};
